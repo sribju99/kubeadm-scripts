33516aa213235a61151a17ba107d09bfd28881fc
sribju99 <146910851+sribju99@users.noreply.github.com>
2023-11-05 15:51:53 -0600

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -39,8 +39,8 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
 # Install Claico Network Plugin Network 
 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.3/manifests/tigera-operator.yaml
 
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.3/manifests/custom-resources.yaml -O
 
 kubectl create -f custom-resources.yaml
